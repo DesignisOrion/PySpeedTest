0e44ce2632116e4f2d8ccf389b7335a1f00eaee5
DesignIsOrion <DesignIsOrion@Gmail.com>
2020-07-13 16:41:56 -0400

    adding files

---
+++ test.py
@@ -0,0 +1,13 @@
+# Author : Orion Ford
+
+# pip install speedtest-cli
+
+from speedtest import Speedtest
+
+st = Speedtest()
+
+print("Download :", st.download())
+print("Upload :", st.upload())
+
+st.get_servers([])
+print("Ping :", st.results.ping)
